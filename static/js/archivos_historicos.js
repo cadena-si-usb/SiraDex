47594f80b372f6017d3d14b4888f1a4764d493af
Arleyn Goncalves <arleyn.g.2712@gmail.com>
2016-10-12 15:14:31 -0430

    (AG) Se pueden eliminar Tipos de Actividad desde la papelera

--- static/js/archivos_historicos.js
+++ static/js/archivos_historicos.js
@@ -4,4 +4,9 @@ $(document).ready(function(){
       var linkRestaurar = $(e.relatedTarget).data('link-restaurar');
       $("#BotonRestaurar").attr("href", linkRestaurar);
   });
+  
+  $('#myModalEliminar').on('show.bs.modal', function(e){
+      var linkEliminar = $(e.relatedTarget).data('link-eliminar');
+      $("#BotonEliminar").attr("href", linkEliminar);
+  });
 });
