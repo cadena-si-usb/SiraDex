094f1796a3d94698a2cc0625d6000a765d272f1d
root <root@debian2.dex.usb.ve>
2017-02-03 09:18:57 -0400

    cambio dentro dels evcer

--- controllers/default.py
+++ controllers/default.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+#-*- coding: utf-8 -*-
 # this file is released under public domain and you can use without limitations
 
 # -------------------------------------------------------------------------
